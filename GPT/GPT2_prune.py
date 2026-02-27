7a76c238b04cd579dc2b918776f355edeb6f4129
herozxb <herozxb@gmail.com>
2026-02-27 11:48:25 +0800

    deepseek good innovation

--- GPT/GPT2_prune.py
+++ GPT/GPT2_prune.py
@@ -46,7 +46,7 @@ device       = 'cuda' if torch.cuda.is_available() else 'cpu'
 eval_iters   = 200
 n_embd       = 128
 n_head       = 8
-n_layer      = 20
+n_layer      = 50
 dropout      = 0.2
 
 def get_batch(split):
